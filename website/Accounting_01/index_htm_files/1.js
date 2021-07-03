ef011c0330aef8ad078ba3080bb66e2afb681d41
Iffy Art <iffyart@i-noby.com>
2021-07-04 01:40:19 +0800

    feat: Accountion

---
+++ website/Accounting_01/index_htm_files/1.js
@@ -0,0 +1,22 @@
+window.local_nivoslider_28_data = [
+{
+ autoscroll: "5",
+ effect: "9"
+},
+{
+ caption: "Caption text...",
+ image: "index_htm_files/52.jpg"
+},
+{
+ caption: "Caption text...",
+ image: "index_htm_files/53.jpg"
+},
+{
+ caption: "Caption text...",
+ image: "index_htm_files/54.jpg"
+},
+{
+ caption: "Caption text...",
+ image: "index_htm_files/55.jpg"
+}
+];
---
+++ website/Accounting_04/index_htm_files/1.js
@@ -0,0 +1,22 @@
+window.local_nivoslider_27_data = [
+{
+ autoscroll: "5",
+ effect: "9"
+},
+{
+ caption: "Caption text...",
+ image: "index_htm_files/64.jpg"
+},
+{
+ caption: "Caption text...",
+ image: "index_htm_files/65.jpg"
+},
+{
+ caption: "Caption text...",
+ image: "index_htm_files/66.jpg"
+},
+{
+ caption: "Caption text...",
+ image: "index_htm_files/67.jpg"
+}
+];
