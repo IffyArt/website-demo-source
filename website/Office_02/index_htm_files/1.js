127ac2a1f869e4e8d83e1042de4dee369cd57513
Iffy Art <iffyart@i-noby.com>
2021-07-04 15:49:09 +0800

    feat: Office pages

---
+++ website/Office_02/index_htm_files/1.js
@@ -0,0 +1,22 @@
+window.local_nivoslider_29_data = [
+{
+ autoscroll: "5",
+ effect: "9"
+},
+{
+ caption: "Caption text...",
+ image: "index_htm_files/54.jpg"
+},
+{
+ caption: "Caption text...",
+ image: "index_htm_files/55.jpg"
+},
+{
+ caption: "Caption text...",
+ image: "index_htm_files/56.jpg"
+},
+{
+ caption: "Caption text...",
+ image: "index_htm_files/57.jpg"
+}
+];
---
+++ website/Office_03/index_htm_files/1.js
@@ -0,0 +1,22 @@
+window.local_nivoslider_32_data = [
+{
+ autoscroll: "5",
+ effect: "9"
+},
+{
+ caption: "Caption text...",
+ image: "index_htm_files/61.jpg"
+},
+{
+ caption: "Caption text...",
+ image: "index_htm_files/62.jpg"
+},
+{
+ caption: "Caption text...",
+ image: "index_htm_files/63.jpg"
+},
+{
+ caption: "Caption text...",
+ image: "index_htm_files/64.jpg"
+}
+];
