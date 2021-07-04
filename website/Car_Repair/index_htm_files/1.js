11dfa4f651454fa74b06bd35ec050fb7d65562f8
Iffy Art <iffyart@i-noby.com>
2021-07-04 15:39:54 +0800

    feat: Car Repair & Dentist & Healthy Food & Homestay & Hotel Industry pages

---
+++ website/Car_Repair/index_htm_files/1.js
@@ -0,0 +1,22 @@
+window.local_nivoslider_7_data = [
+{
+ autoscroll: "5",
+ effect: "9"
+},
+{
+ caption: "Caption text...",
+ image: "index_htm_files/79.jpg"
+},
+{
+ caption: "Caption text...",
+ image: "index_htm_files/80.jpg"
+},
+{
+ caption: "Caption text...",
+ image: "index_htm_files/81.jpg"
+},
+{
+ caption: "Caption text...",
+ image: "index_htm_files/82.jpg"
+}
+];
