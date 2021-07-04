dba76e61c44b002dc2d8eb7a80c8538054a457b3
Iffy Art <iffyart@i-noby.com>
2021-07-04 15:05:40 +0800

    feat: bicycle sales

---
+++ website/Bicycle_Sales_01/index_htm_files/1.js
@@ -0,0 +1,22 @@
+window.local_nivoslider_7_data = [
+{
+ autoscroll: "5",
+ effect: "9"
+},
+{
+ caption: "Caption text...",
+ image: "index_htm_files/113.jpg"
+},
+{
+ caption: "Caption text...",
+ image: "index_htm_files/114.jpg"
+},
+{
+ caption: "Caption text...",
+ image: "index_htm_files/115.jpg"
+},
+{
+ caption: "Caption text...",
+ image: "index_htm_files/116.jpg"
+}
+];
---
+++ website/Bicycle_Sales_02/index_htm_files/1.js
@@ -0,0 +1,17 @@
+window.local_cycleFade_v8_1_data = [
+{
+ image: "index_htm_files/13.jpg"
+},
+{
+ image: "index_htm_files/14.jpg"
+},
+{
+ image: "index_htm_files/15.jpg"
+},
+{
+ image: "index_htm_files/16.jpg"
+},
+{
+ image: "index_htm_files/17.jpg"
+}
+];
