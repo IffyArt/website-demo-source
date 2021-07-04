79d7fb28ed69c9b4c1f10db475fc10e7e4a3750a
Iffy Art <iffyart@i-noby.com>
2021-07-04 14:58:52 +0800

    feat: Advisory & Dance & Boutique pages

---
+++ website/Boutique_Industry/index_htm_files/1.js
@@ -0,0 +1,14 @@
+window.local_mosaic_panel_6_data = [
+{
+ direction: "1",
+ speed: "0.5",
+ theme: "1",
+ trans: "0.9"
+},
+{
+ heading: "index_htm_files/38.png",
+ image: "index_htm_files/39.jpg",
+ link: "#",
+ text: "index_htm_files/40.png"
+}
+];
